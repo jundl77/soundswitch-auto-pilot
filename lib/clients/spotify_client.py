fb2850d1c6b8c3b6a0b1060169a1aaf6d2428fcb
Julian Brendl <julianbrendl@gmail.com>
2022-06-12 19:53:29 +0200

    made spotify optional

--- lib/clients/spotify_client.py
+++ lib/clients/spotify_client.py
@@ -1,6 +1,7 @@
 import spotipy
 import json
 import logging
+from typing import Optional
 from pathlib import Path
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -19,16 +20,22 @@ class SpotifyTrackAnalysis:
 
 class SpotifyClient:
     def __init__(self):
-        self.spotify_details = self._read_spotify_details_file()
-        auth_manager = SpotifyOAuth(client_id=self.spotify_details.client_id,
-                                    client_secret=self.spotify_details.client_secret,
-                                    redirect_uri="http://localhost:8877/callback",
-                                    scope="user-read-playback-state")
-        self.spotify = spotipy.Spotify(auth_manager=auth_manager)
+        spotify_details = self._read_spotify_details_file()
+        if spotify_details is not None:
+            auth_manager = SpotifyOAuth(client_id=spotify_details.client_id,
+                                        client_secret=spotify_details.client_secret,
+                                        redirect_uri="http://localhost:8877/callback",
+                                        scope="user-read-playback-state")
+            self.spotify = spotipy.Spotify(auth_manager=auth_manager)
+            self.is_active = True
+        else:
+            self.is_active = False
 
-    def _read_spotify_details_file(self) -> SpotifyDetails:
+    def _read_spotify_details_file(self) -> Optional[SpotifyDetails]:
         spotify_details_file_path = (Path(__file__).parent.parent.parent / "spotify_details.json").absolute()
-        assert spotify_details_file_path.is_file(), f"{spotify_details_file_path} does not exist"
+        if not spotify_details_file_path.is_file():
+            logging.info(f"[spotify] {spotify_details_file_path} does not exist, will not use spotify")
+            return None
 
         with open(spotify_details_file_path, 'r') as f:
             spotify_details_json = json.load(f)
@@ -36,9 +43,15 @@ class SpotifyClient:
         return SpotifyDetails(spotify_client_id=spotify_details_json['spotify_client_id'],
                               spotify_client_secret=spotify_details_json['spotify_client_secret'])
 
+    def is_active(self) -> bool:
+        return self.is_active
+
     def get_current_song_analysis(self) -> SpotifyTrackAnalysis:
+        if not self.is_active:
+            return SpotifyTrackAnalysis('', 0)
+
         current_playback = self.spotify.current_playback()
-        if not current_playback['is_playing']:
+        if current_playback is None or not current_playback['is_playing']:
             logging.error("[spotify] song is not playing, returning default values")
             return SpotifyTrackAnalysis('', 0)
         track_name = current_playback['item']['name']
