ec1a1ac909fb7c837eabea38a195f347ea8c2bdf
Julian Brendl <julianbrendl@gmail.com>
2023-06-13 02:08:48 +0200

    actually set UV light

--- lib/clients/overlay_definitions.py
+++ lib/clients/overlay_definitions.py
@@ -7,11 +7,12 @@ class OverlayEffect(Enum):
 
 class OverlayDefinition:
     def __init__(self, start_offset: int, dmx_data: list[int]):
+        """ start_offset is 0-indexed """
         assert 0 < start_offset + len(dmx_data) <= 512
         self.start_offset: int = start_offset
         self.dmx_data: list[int] = dmx_data
 
 
 OVERLAY_EFFECTS = {
-    OverlayEffect.UV_LIGHT: OverlayDefinition(64, [128] * 32)
+    OverlayEffect.UV_LIGHT: OverlayDefinition(64, [0, 0, 0, 0, 255, 0, 79, 0] * 4)
 }
